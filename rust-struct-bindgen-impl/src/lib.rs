2811bfed3d8604568c51dc1011f7a815213be8cf
officeyutong <yt.xyxx@gmail.com>
2023-04-20 04:30:03 +0000

    Add README an docs

--- rust-struct-bindgen-impl/src/lib.rs
+++ rust-struct-bindgen-impl/src/lib.rs
@@ -1,3 +1,32 @@
+//!
+//! # Bindgen implementation
+//! Here is the core implementation of `rust-struct-bindgen`, which accepts a BTF or ELF file and yields the TokenStream-represented rust source.
+//!
+//! # The generating stragety
+//!
+//! - For every `struct`, `enum`, `int`, `float`, `array`, `ptr`, `rust-struct-bindgen` will generate a represented type named `btf_type_XX`, where `XX` is the btf type id of the corresponding type for them. And the corresponding serializing and deserializing function will also be generated.  
+//! - Serializing functions always have signature like `fn (&T) -> Result<Vec<u8>, String>`where `T` is the generated rust type. The function name will always be `deserialize_btf_type_XX`, where `XX` is the type id. This function will be in the `inner_impl` module.
+//! - Deserializing functions have signature like `fn (&[u8]) -> Result<T, String>`, `T` is also the generated rust type. The function name will always be `serialize_btf_type_XX`, where `XX` is the type id. This function will be in the `inner_impl` module.
+//! - struct and enums will have a type alias pointing to the `btf_type_XX` type. The alias will be its original name. And deserializing function and serializing function will also be associated functions of the alias type.
+//! - The alias of `struct` and `enums` will be put at the top level module. All other type definitions and (de)serializing functions will be put under a module named `inner_impl`; All things are `pub`.
+//! e.g, for a btf int with sz 64bits and unsigned encoding and typeid `1`, `rust-struct-bindgen` will generate the following code:
+//! ```rust
+//!    #[allow(unused)]
+//!    #[allow(non_camel_case_types)]
+//!    pub type btf_type_1 = u64;
+//!    #[allow(unused)]
+//!    pub fn deserialize_btf_type_1(b: &[u8]) -> Result<btf_type_1, String> {
+//!        if b.len() != 8 {
+//!            return Err("Expected a slice with 8 bytes".to_string());
+//!        }
+//!        Ok(u64::from_ne_bytes(b.try_into().unwrap()))
+//!    }
+//!    #[allow(unused)]
+//!    pub fn serialize_btf_type_1(v: &btf_type_1) -> Result<Vec<u8>, String> {
+//!        Ok(v.to_ne_bytes().to_vec())
+//!    }
+//!```
+
 use anyhow::Result;
 use btf::types::{Btf, BtfType};
 use cache::SizeResolveCache;
@@ -16,7 +45,7 @@ pub(crate) mod cache;
 /// Some helper functions
 pub mod helper;
 pub(crate) mod types;
-
+/// Generate a TokenStream for the specified Btf
 pub fn generate_bindgen_token_stream(btf: &Btf) -> Result<TokenStream> {
     let mut inner_impl = TokenStream::new();
     let mut outer_impl = TokenStream::new();
